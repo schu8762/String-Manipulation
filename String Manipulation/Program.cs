97a79bb9455b6497b8333b71fece270664d56b82
schu8762 <J.Schuller@ou.edu>
2019-10-01 11:29:40 -0500

    blah

--- String Manipulation/Program.cs
+++ String Manipulation/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//@author Jacob Schuller
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,40 @@ namespace String_Manipulation
 {
     class Program
     {
+        static string quote = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots.So far, the universe is winning.";
+
         static void Main(string[] args)
         {
+            Console.WriteLine(quote);
+
+            Console.WriteLine("What's your word?>>");
+            string userWord = Console.ReadLine();
+
+            Console.WriteLine($"What do you want to replace your {userWord} with?>>");
+            string replaceWord = Console.ReadLine();
+
+            if (quote.Contains(userWord))
+            {
+               string replaceQuote = quote.Replace(userWord, replaceWord);
+                Console.WriteLine(replaceQuote);
+            }
+
+            else
+            {
+                //This spits out your answer backwards
+                Console.WriteLine($"Sorry, I could not find your word, {userWord}.");
+                for (int i = userWord.Length - 1; i > 0; i--)
+                {
+                    Console.Write(userWord[i]);
+                }
+            }
+            
+
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
